ce90467300549d617b8db7baef166206fcc9bf81
bryant tunbutr <bryantt23@users.noreply.github.com>
2021-09-12 23:59:40 -0400

    Change BoardSection into a functional component

--- src/components/BoardSection.js
+++ src/components/BoardSection.js
@@ -1,44 +1,46 @@
 import React, { Component } from 'react';
 
-export class BoardSection extends Component {
+function BoardSection({
+  attacked,
+  status,
+  isComputer,
+  updateBoardSectionState
+}) {
   ///maybe deal with it differently based on player or computer board
-  render() {
-    const { attacked, status, isComputer } = this.props;
-    let message, color;
-    if (status === undefined || (!attacked && isComputer)) {
-      message = 'SEA';
-      color = 'blue';
-    } else if (typeof status === 'object') {
-      message = 'SHIP';
-      color = 'grey';
+  let message, color;
+  if (status === undefined || (!attacked && isComputer)) {
+    message = 'SEA';
+    color = 'blue';
+  } else if (typeof status === 'object') {
+    message = 'SHIP';
+    color = 'grey';
+  } else {
+    message = status; //should be a string
+    if (status === 'HIT') {
+      color = 'red';
     } else {
-      message = status; //should be a string
-      if (status === 'HIT') {
-        color = 'red';
-      } else {
-        color = 'green';
-      }
+      color = 'green';
     }
-
-    // console.log(message);
-    return (
-      <td
-        disabled={attacked}
-        style={{
-          height: 50,
-          width: 50,
-          display: 'inlineBlock',
-          backgroundColor: color
-        }}
-        onClick={() => {
-          // https:stackoverflow.com/questions/51549115/best-way-to-disabled-div-onclick-in-react
-          !attacked && this.props.updateBoardSectionState();
-        }}
-      >
-        {message}
-      </td>
-    );
   }
+
+  // console.log(message);
+  return (
+    <td
+      disabled={attacked}
+      style={{
+        height: 50,
+        width: 50,
+        display: 'inlineBlock',
+        backgroundColor: color
+      }}
+      onClick={() => {
+        // https:stackoverflow.com/questions/51549115/best-way-to-disabled-div-onclick-in-react
+        !attacked && updateBoardSectionState();
+      }}
+    >
+      {message}
+    </td>
+  );
 }
 
 export default BoardSection;
